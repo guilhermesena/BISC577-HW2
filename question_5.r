4684791395d4c9877119681183351cf29d9fe6f5
Sena <desenabr@usc.edu>
2016-05-09 15:55:51 -0700

    Improving graph on question 5

--- question_5.r
+++ question_5.r
@@ -4,10 +4,11 @@ library(stats)
 
 # I wrote the previous rsq values on a txt file
 rsq <- read.table("in/rsq.txt")
+y_x <- seq(0.76, 0.87, 0.0001)
+plot(y_x, y_x, xlab = "1-mer", ylab = "1-mer+shape",  type = "l")
+points(rsq[1,], rsq[2,], pch=17:19, col=c("red", "green", "blue"))
+legend(0.84, 0.78, c("Mad", "Max", "Myc"), pch = 17:19, col=c("red", "green", "blue"))
 
-# Plot the values
-plot(as.numeric(rsq[1,]), as.numeric(rsq[2,]), xlab = "1-mer", ylab = "1-mer+shape", 
-     col=c("red","green","blue"), pch=15:17)
 
 #Wilcox's rank sum test
 wilcox.test(as.numeric(rsq[1,]), as.numeric(rsq[2,]), paired=FALSE, alternative="greater")
